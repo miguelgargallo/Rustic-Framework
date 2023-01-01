c3a34b03c7294da289d8c4134ec585ee4e53096b
Miguel Gargallo <mgargallo16@gmail.com>
2023-01-02 00:56:31 +0100

    1.0.0 Source Code

--- do.sh
+++ do.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
-# Install Rust if it is not already installed
-if ! command -v rustc &>/dev/null; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-fi
-
 # Create a new Rust project
 cargo new --bin rust-webserver
 cd rust-webserver
 
-# Add the following dependencies to the project's `Cargo.toml` file:
-# actix-web = "2.0"
-# env_logger = "0.7"
+# Check for the latest compatible versions of the `actix-web`, `env_logger`, and `actix-rt` crates
+actix_web_version=$(cargo search --limit=1 actix-web | grep -oP '^actix-web\s+\K[^\s]+')
+env_logger_version=$(cargo search --limit=1 env_logger | grep -oP '^env_logger\s+\K[^\s]+')
+actix_rt_version=$(cargo search --limit=1 actix-rt | grep -oP '^actix-rt\s+\K[^\s]+')
 
-# Create a file `src/main.rs` with the following contents:
+# Add the necessary dependencies to the project's `Cargo.toml` file
+echo "
+[dependencies]
+actix-web = \"$actix_web_version\"
+env_logger = \"$env_logger_version\"
+actix-rt = \"$actix_rt_version\"
+" >>Cargo.toml
+
+# Create a file `src/main.rs` with the code for the web server
 echo 'use actix_web::{web, App, HttpResponse, HttpServer};
 use env_logger;
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     env_logger::init();
     HttpServer::new(|| {
         App::new()
@@ -26,7 +29,6 @@ async fn main() -> std::io::Result<()> {
     })
     .bind("127.0.0.1:3000")?
     .run()
-    .await
 }' >src/main.rs
 
 # Build and run the server
